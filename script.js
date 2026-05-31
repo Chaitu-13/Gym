e1674f795c111026618fc49b3ec96b81e4a6308b
Chay-30 <chappidichaitanyakumar@gmail.com>
2026-05-31 11:48:24 +0530

    Gym

---
+++ script.js
@@ -0,0 +1,31 @@
+function calculateBMI(){
+
+let height =
+document.getElementById("height").value;
+
+let weight =
+document.getElementById("weight").value;
+
+height = height / 100;
+
+let bmi =
+(weight / (height * height)).toFixed(2);
+
+let status = "";
+
+if(bmi < 18.5){
+status = "Underweight";
+}
+else if(bmi < 25){
+status = "Normal";
+}
+else if(bmi < 30){
+status = "Overweight";
+}
+else{
+status = "Obese";
+}
+
+document.getElementById("result").innerHTML =
+`BMI: ${bmi} (${status})`;
+}
